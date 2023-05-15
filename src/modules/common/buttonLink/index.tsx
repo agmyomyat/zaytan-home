ac096ef0e4ed947a09f1a2abbd45c2d219b57213
urnotalone2 <54217097+agmyomyat@users.noreply.github.com>
2023-05-15 23:43:44 +0630

    chore: make tow button link components external and internal

--- src/modules/common/buttonLink/index.tsx
+++ src/modules/common/buttonLink/index.tsx
@@ -1,6 +1,7 @@
 import { Button, Anchor, MantineSize, ButtonProps } from '@mantine/core';
+import Link from 'next/link';
 import { HTMLAttributeAnchorTarget } from 'react';
-export const ButtonLink: React.FC<
+export const ButtonExternalLink: React.FC<
   {
     children: React.ReactNode;
     size?: MantineSize;
@@ -23,5 +24,30 @@ export const ButtonLink: React.FC<
         {children}
       </Anchor>
     </Button>
+  );
+};
+export const ButtonInternalLink: React.FC<
+  {
+    children: React.ReactNode;
+    size?: MantineSize;
+    className?: string;
+    href: string;
+    target?: HTMLAttributeAnchorTarget | undefined;
+  } & ButtonProps
+> = ({ className, children, size, href, target, ...rest }) => {
+  return (
+    <Link
+      href={href}
+      className="w-full text-white no-underline"
+      target={target}
+    >
+      <Button
+        size={size || 'md'}
+        classNames={{ root: 'hover:!bg-gray-700 bg-gray-900 rounded-lg' }}
+        {...rest}
+      >
+        {children}
+      </Button>
+    </Link>
   );
 };
