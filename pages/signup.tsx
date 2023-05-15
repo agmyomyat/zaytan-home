ddb6da6c23e3901069d714a7c1929d288ae7cb06
urnotalone2 <54217097+agmyomyat@users.noreply.github.com>
2023-05-15 22:53:34 +0630

    chore:add meta

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -3,8 +3,14 @@ import { Layout } from '@/modules/layout/templates';
 
 export default function SignUp() {
   return (
-    <Layout>
-      <SignUpTemplatePage />
-    </Layout>
+    <>
+      <title>Sign up</title>
+      <meta name="description" content="Zaytan Sign up" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <Layout>
+        <SignUpTemplatePage />
+      </Layout>
+    </>
   );
 }
