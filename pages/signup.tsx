5691cee8167cdd405cee29a7fbe74b852b5d7969
aung myo myat <dev.server.mm@gmail.com>
2023-06-18 16:20:18 +0630

    chore:add meta data in signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,13 +1,16 @@
 import { SignUpTemplatePage } from '@/modules/account/templates';
 import { Layout } from '@/modules/layout/templates';
+import Head from 'next/head';
 
 export default function SignUp() {
   return (
     <>
-      <title>Sign up</title>
-      <meta name="description" content="Zaytan Sign up" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <Head>
+        <title>Sign up</title>
+        <meta name="description" content="Zaytan Sign up" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Layout>
         <SignUpTemplatePage />
       </Layout>
